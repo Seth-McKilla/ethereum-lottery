814adffd8c693d534ec1b07728b559fbdb12a53b
Seth McCullough <seth@endevrs.dev>
2021-09-12 11:11:06 -0500

    Add testing setup

--- compile.js
+++ compile.js
@@ -2,7 +2,24 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const lotteryPath = path.resolve(__dirname, "contracts", "Lottery");
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+const input = {
+  language: "Solidity",
+  sources: {
+    "Lottery.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+module.exports = output.contracts["Lottery.sol"].Lottery;
